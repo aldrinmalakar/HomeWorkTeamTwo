52c57e687f526744b47de22d8d256980f49c9215
Aldrin <aldrinmalakar@gmail.com>
2020-10-22 13:10:23 -0400

    Selenium Worked

--- src/HomeWorkOne_20TestCases/aldrinMalakar.java
+++ src/HomeWorkOne_20TestCases/aldrinMalakar.java
@@ -1,4 +1,14 @@
 package HomeWorkOne_20TestCases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class aldrinMalakar {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","BrowserDriver/windows/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.udemy.com/");
+        System.out.println(driver.getTitle());
+        driver.close();
+    }
 }
